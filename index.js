4c66da2c88c3fea3c64dd7e6e168a37f2f8fbdde
NEHADEC <faizanbilal472@gmail.com>
2024-06-05 06:30:54 -0700

    Adding Number-Guessing- Game Typescript

---
+++ index.js
@@ -0,0 +1,17 @@
+#! /usr/bin.env node
+import inquirer from "inquirer";
+console.log("\n\twelcome to Code With Neha - CLI Number Gussing Game\t");
+const randomNumber = Math.floor(Math.random() * 5 + 1);
+const answer = await inquirer.prompt([
+    {
+        name: "userGuessNumber",
+        type: "number",
+        message: "Enter Your Guess Number(Number Limit From 1to 5):",
+    },
+]);
+if (answer.userGuessNumber === randomNumber) {
+    console.log("congratulation ! you guess a correct number.");
+}
+else {
+    console.log("sorry, you guess a wrong number");
+}
